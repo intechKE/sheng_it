8fa9f18bed1b2cf394ea392e0cb1c9b9668f6a70
shadmeoli <shadmeoli@gmail.com>
2022-10-04 20:46:11 +0300

    Reinforced Architecture Design

---
+++ App/Data/URL_Flags.py
@@ -0,0 +1,9 @@
+# this contains the sites to set up crawlers for the "other_sources_crawler"
+# [TODO] -> add-up anyother site that will have great training sets.
+
+
+URL_FLAGS = [
+    # this URL is the exact route to the page to set crawlers.
+    "https://kenyanmagazine.co.ke/200-sheng-words-and-their-meanings/" 
+
+]
--- App/Data/__init__.py
+++ App/Data/__init__.py
@@ -1,1 +1,7 @@
 # [TODO] -> Add data sets here
+
+# calling all the creawlers
+from facebook_Crawler import *
+from lirics_Crawler import *
+from other_sources_Crawler import *
+from twitter_Crawler import *
